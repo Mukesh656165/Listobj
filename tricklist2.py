b800b6ae4507b357626404991505c0a830a0e2e2
MUKESH <mukesh.ora@outlook.com>
2019-09-06 00:33:21 +0530

    lines of code added

---
+++ tricklist2.py
@@ -0,0 +1,10 @@
+# start:stop:step slice in list
+a_list = ['kingsman','python','got','darkphonix','javascript','html']
+
+print(a_list[0:4:2])
+
+# to print [python,darkphonix,html]
+print(a_list[1:6:2])
+print(a_list[6:0:-1])
+# syntax for reversing a string
+print(a_list[::-1])
