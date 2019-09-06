bef9e7a7c1131c98bd6987de8671574be0c06481
MUKESH <mukesh.ora@outlook.com>
2019-09-06 20:13:07 +0530

    Lines added to

---
+++ tricklist3.py
@@ -0,0 +1,43 @@
+# something to note
+# if s is string,s[:] it returns  a reference of the same object
+# s = 'mukesh'
+# print(s[:])
+# print(s[:] is s)
+
+# lets make some change
+# s1 = ['mukesh']
+# print(s1[:])
+# print(s1[:] is s1)
+#
+# print(id(s[:]))
+# print(id(s))
+#
+# print(id(s1[:]))
+# print(id(s1))
+
+# if a is list it returns a new object that is copy of a
+
+# membership test in a list
+# a = ['superman','spiderman','batman','ironman']
+# print('superman' in a)
+# print('skyfall' not in a)
+
+# concatenation and replication in list
+# a = ['apple','grape','orange','mousamb']
+# print(a +['milk','fish'])
+#
+# print(a*2)
+
+# list without assign to any variable
+print(['age','group','cast','human'] [:2])
+
+# to reverse this list
+print(['age','group','cast','human'] [::-1])
+
+#concatenate 2 strings
+print(['age','height']+['weight','name'])
+print(len(['ab','bc']))
+
+#to resverse  astring
+print('Mukesh Kumar'[::-1])
+
