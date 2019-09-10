36d7e913b139cc1a4f0406d55592c0bc00691dac
MUKESH <mukesh.ora@outlook.com>
2019-09-11 02:53:49 +0530

    one more list file created by freakin Mukesh

---
+++ triclist5.py
@@ -0,0 +1,20 @@
+'we can create list without assign it any variable'
+['mukesh','jason','raj','erick','roman','undertaker']
+print(['mukesh','jason','raj','erick','roman','undertaker'][2])
+
+'we can reverse it also'
+print(['mukesh','jason','raj','erick','roman','undertaker'][::-1])
+
+'checking of membership'
+print('undertaker' in['mukesh','jason','raj','erick','roman','undertaker'])
+
+'we can add one more list in this list also'
+print(['sachin','ramos']+['mukesh','jason','raj','erick','roman','undertaker']) #it is adding it in 1st
+
+'if you want to add a list in last'
+print(['mukesh','jason','raj','erick','roman','undertaker'] +['ronaldo','pedolski'])
+print(len(['mukesh','jason','raj','erick','roman','undertaker']))
+print(len(['mukesh','jason','raj','erick','roman','undertaker'][::-1]))
+print(len(['mukesh','jason','raj','erick','roman','undertaker'][1]))# it is counting from jason to onwards
+#how many items remaining inside a list
+'trick point'
